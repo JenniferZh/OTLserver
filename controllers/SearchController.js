8b38785c5bd12d96e4d0c8a60a00d0a7949085af
JenniferZh <jenniferzhang0606@gmail.com>
2017-11-17 00:19:31 +0800

    search get search content

--- controllers/SearchController.js
+++ controllers/SearchController.js
@@ -3,13 +3,16 @@ var async = require('async');
 
 
 exports.search_detail = function (req, res, next) {
-    req.checkBody('otlname', 'Name required').notEmpty();
-    var errors = req.validationErrors();
-    if(errors) {
-        res.render('search', {title: 'ere'});
-        return;
-    }
+    // req.checkBody('otlname', 'Name required').notEmpty();
+    // var errors = req.validationErrors();
+    // if(errors) {
+    //     res.render('search', {title: 'ere'});
+    //     return;
+    // }
 
-    res.render('search', {title:'not ere'});
+    //res.send(req);
+    //console.log(req.params);
+
+    res.render('search', {title:req.body.name_field});
 
 };
