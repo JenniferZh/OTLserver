818065c30e2a2de9c0df803a76e89abb1f360a3f
JenniferZh <jenniferzhang0606@gmail.com>
2017-11-17 15:06:49 +0800

    search version 2

--- controllers/SearchController.js
+++ controllers/SearchController.js
@@ -13,6 +13,13 @@ exports.search_detail = function (req, res, next) {
     //res.send(req);
     //console.log(req.params);
 
-    res.render('search', {title:req.body.name_field});
+    //res.render('search', {title:req.body.name_field});
+    var regexstring = '\''+req.body.name_field +'\'';
+    //console.log(regexstring);
+    Item.find({name: new RegExp(req.body.name_field,"i")}, function(err, entity) {
+        if (err) return handleError(err);
+        console.log(entity.length);
+        res.render('search', {search_result: entity});
+    });
 
 };
