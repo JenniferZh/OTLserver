3ccd17dbde4a6dad3a62e678ed71b89389e2a841
JenniferZh <jenniferzhang0606@gmail.com>
2018-05-23 14:20:36 +0800

    +same search for multiscope

--- controllers/SearchController.js
+++ controllers/SearchController.js
@@ -2,6 +2,7 @@ var Item = require('../models/otlentity');
 var RoadItem = require('../models/roaditem');
 var RoadClassItem = require('../models/roadclassitem');
 var RailClassItem = require('../models/classitem');
+var AllScope = require('../models/allscope');
 var async = require('async');
 
 
@@ -62,8 +63,20 @@ exports.search_detail = function (req, res, next) {
                     //res.render('search', {search_result: entity});
                 });
 
-            }
+            },
+            function (callback) {
+                AllScope.find({name: new RegExp(req.body.name_field,"i")}, function(err, entity) {
+                    if (err) return handleError(err);
+                    //console.log(entity.length);
+                    entity.forEach(function(item){
+                        item.from = '多领域分类编码';
+                    });
+                    callback(null, entity);
 
+                    //res.render('search', {search_result: entity});
+                });
+
+            }
 
     ],
     function(err, result){
@@ -71,7 +84,7 @@ exports.search_detail = function (req, res, next) {
         result.forEach(function(element){
             final_result = final_result.concat(element);
         });
-        console.log(final_result);
+
 
         res.render('search', {search_result: final_result});
     });
